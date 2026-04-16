553bada5b5e9924cb5711dd9f77e1665f91d1392
André Bellandi Vital Rodrigues <decobegoo2@gmail.com>
2026-04-16 17:48:41 -0300

    melhoria no BibliotecaContext

--- Biblioteca.Infrastructure/BibliotecaContext.cs
+++ Biblioteca.Infrastructure/BibliotecaContext.cs
@@ -6,9 +6,19 @@ namespace Biblioteca.Infrastructure;
 public class BibliotecaContext : DbContext
 {
     public DbSet<Livro> Livros { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<CategoriaLivro> CategoriasLivros { get; set; }
+    public DbSet<Emprestimo> Emprestimos { get; set; }
+    public DbSet<Feedback> Feedbacks { get; set; }
 
     public BibliotecaContext(DbContextOptions<BibliotecaContext> options)
         : base(options)
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(BibliotecaContext).Assembly);
     }
 }
